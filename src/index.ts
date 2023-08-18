da8ab92ff957697a3e84d915a274dce99237e86c
Navin Mishra <navinmishra1717@gmail.com>
2023-08-18 17:47:24 +0545

    nginx conf and dockerfile added

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
 import 'dotenv/config';
 import App from '@app/app';
-import cluster from 'cluster';
-import * as OS from 'os';
+// import cluster from 'cluster';
+// import * as OS from 'os';
 
 (async () => {
-  if (cluster.isPrimary) {
-    const numWorkers = OS.cpus().length;
-    console.log(`numWorkers: ${numWorkers}`);
-    for (let i = 0; i < numWorkers; i += 1) {
-      // for optimum performance
-      cluster.fork();
+  // if (cluster.isPrimary) {
+  //   const numWorkers = OS.cpus().length;
+  //   console.log(`numWorkers: ${numWorkers}`);
+  //   for (let i = 0; i < numWorkers; i += 1) {
+  //     // for optimum performance
+  //     cluster.fork();
+  //   }
+  //   cluster.on('online', (worker) => {
+  //     console.log(`worker: ${worker.process.pid} is online`);
+  //   });
+  //   cluster.on('exit', (worker, code, signal) => {
+  //     console.log(
+  //       `worker: ${worker.process.pid} exited with code ${code} and signal ${signal}`,
+  //     );
+  //     console.log('Starting a new worker');
+  //     cluster.fork();
+  //   });
+  // } else {
+  const app = await App();
+  app.listen(process.env.APP_PORT, () => {
+    console.log(`Server started on port ${process.env.APP_PORT}`);
+    if (process.send) {
+      process.send('ready', () => console.log('Hello buddy'));
     }
-    cluster.on('online', (worker) => {
-      console.log(`worker: ${worker.process.pid} is online`);
-    });
-    cluster.on('exit', (worker, code, signal) => {
-      console.log(
-        `worker: ${worker.process.pid} exited with code ${code} and signal ${signal}`,
-      );
-      console.log('Starting a new worker');
-      cluster.fork();
-    });
-  } else {
-    const app = await App();
-    app.listen(process.env.APP_PORT, () => {
-      console.log(`Server started on port ${process.env.APP_PORT}`);
-      if (process.send) {
-        process.send('ready', () => console.log('Hello buddy'));
-      }
-    });
-  }
+  });
+  // }
 })();
